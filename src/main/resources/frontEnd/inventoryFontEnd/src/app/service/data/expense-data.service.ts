7fc6fbbfa5b90b5140476a1f5ec0e8ceb49f24f2
Yusuf <common.dsdl@gmail.com>
2019-04-19 02:06:09 +0600

    Expense page is done

--- src/main/resources/frontEnd/inventoryFontEnd/src/app/service/data/expense-data.service.ts
+++ src/main/resources/frontEnd/inventoryFontEnd/src/app/service/data/expense-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Expense } from 'src/app/expense/expense.component';
+import { formatDate } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -20,11 +21,15 @@ export class ExpenseDataService {
     let date = new Date();
     let strDate = new Date(date.getFullYear(), date.getMonth(), 1);
     let endDate = new Date(date.getFullYear(), date.getMonth()+1, 0);
-    const  format = 'dd-MM-yyyy';
+    const  format = 'dd-MM-yyyy'; 
+    const locale = 'en-US';
+    const formattedStrDate = formatDate(strDate, format, locale);
+    const formattedEndDate = formatDate(endDate, format, locale);
 
-    console.log("DATE "+strDate );
-    console.log("END DATE "+endDate);
-    return this.http.get<Expense[]>(this.baseUrl+'/expensesbydate/get?page='+page+'&size=10&strDate=01-04-2019&endDate=30-04-2019')
+     
+    console.log("DATE "+formattedStrDate );
+    console.log("END DATE "+formattedEndDate);
+    return this.http.get<Expense[]>(this.baseUrl+'/expensesbydate/get?page='+page+'&size=2&strDate='+formattedStrDate+'&endDate='+formattedEndDate)
   }
 
   deleteExpenseService(id){
